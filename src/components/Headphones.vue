c261a9a10813664c0330bf730f28314daea048af
JimenaMartin <jimena.martin@masmovil.com>
2024-10-24 16:13:32 +0200

    headphones color selection

---
+++ src/components/Headphones.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { watch } from 'vue';
+import { useGLTF, Levioso, Html } from '@tresjs/cientos';
+import { useState } from '../composables/state';
+
+const { nodes, materials } = await useGLTF('/models/headphones/headphones.gltf');
+
+const model = nodes['Headphones_7'];
+
+const { selectedBaseColor, selectedCushionColor } = useState()
+
+watch(selectedBaseColor, (color) => {
+    materials.Base.color.set(color);
+});
+
+watch(selectedCushionColor, (color) => {
+    materials.Cush.color.set(color);
+});
+
+</script>
+
+<template>
+    <Levioso>
+        <primitive :object="model" />
+    </Levioso>
+</template>
